1015b1d6a04824eef8890d43314f30ba4ce916c5
hohuynhnhu <hhnhu2003@gmail.com>
2025-08-08 17:17:33 +0700

    update

--- plugins/vuefire.ts
+++ plugins/vuefire.ts
@@ -1,13 +1,4 @@
-// plugins/vuefire.ts
-// import { VueFire, VueFireAuth } from 'vuefire'
-// import { firebaseApp } from '@/plugins/firebase'
 
-// export default defineNuxtPlugin((nuxtApp) => {
-//   nuxtApp.vueApp.use(VueFire, {
-//     firebaseApp,
-//     modules: [VueFireAuth()],
-//   })
-// })
 
 import { firebaseApp } from './firebase'
 import { VueFire, VueFireAuth } from 'vuefire'
